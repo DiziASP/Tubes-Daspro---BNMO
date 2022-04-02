a422832aac1200c9ba1e07cb91344d3e0506f4a2
Dizi <58800976+DiziASP@users.noreply.github.com>
2022-04-03 01:54:57 +0700

    Comment Important File

--- Code/F03.py
+++ Code/F03.py
@@ -1,4 +1,1 @@
-import os
-
-if __name__ == '__main__':
-    print("Hello World")
+# Login
--- Code/base.py
+++ Code/base.py
@@ -1,4 +1,1 @@
-import os
-
-if __name__ == '__main__':
-    print("Hello World")
+# Isinya fungsi2 sama variabel
