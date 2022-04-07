9387660c61f37e9943cd90b4967f59d3dd56de9a
Dizi <58800976+DiziASP@users.noreply.github.com>
2022-04-08 00:08:40 +0700

    After Purge

--- Code/temp_const.py
+++
@@ -1,9 +0,0 @@
-global user
-user = [[1, 'daspro1', 'A', '1234', 'admin', 20000],
-        [2, 'daspro2', 'B', '1234', 'admin', 20000],
-        [3, 'daspro3', 'C', '1234', 'user', 20000],
-        [4, 'daspro4', 'D', '1234', 'user', 20000],
-        [5, 'daspro5', 'E', '1234', 'user', 20000],
-        [6, 'daspro6', 'F', '1234', 'user', 20000],
-        [7, 'daspro7', 'G', '1234', 'user', 20000]
-        ]
