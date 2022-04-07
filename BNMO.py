79d1dda9687bc1a1e19dfc63e19080ac18b78667
Dizi <58800976+DiziASP@users.noreply.github.com>
2022-04-08 01:48:19 +0700

    Finished Register F02

--- BNMO.py
+++ BNMO.py
@@ -1,7 +1,12 @@
 import base
+import F02
+# import F03
+# import F11
+# import F14
+from constant import user
+
 
 if __name__ == "__main__":
     """
         Running the Main Program
     """
-    print(base.length("Anjay"))
--- base.py
+++ base.py
@@ -17,5 +17,60 @@ def length(arr: list) -> int:
     return count
 
 
-def addObj(arr: list, param) -> list:
-    return arr + [param]
+def addObj(list_par: list, param) -> int:
+    list_par += [param]
+    return list_par
+
+
+def conUpper(string: str) -> bool:
+    res = False
+    for each in string:
+        if each == each.upper():
+            res = True
+            break
+    return res
+
+
+def conLower(string: str) -> bool:
+    res = False
+    for each in string:
+        if each == each.lower():
+            res = True
+            break
+    return res
+
+
+def conDigit(string: str) -> bool:
+    res = False
+    num = ['0', '1', '2', '3', '4', '5',
+           '6', '7', '8', '9']
+    for each in string:
+        if each in num:
+            res = True
+            break
+    return res
+
+
+def conSpec(string: str) -> bool:
+    res = False
+    special = ["-", "_"]
+    for each in string:
+        if each in special:
+            res = True
+            break
+    return res
+
+
+def validatePassword():
+    pass
+
+
+def validateUser():
+    while True:
+        username_ = input("Masukkan Username: ")
+        if not((conUpper(username_) or conLower(username_))
+               and conSpec(username_)
+               and conDigit(username_)):
+            print("Username tidak sesuai dengan ketentuan. Silahkan coba kembali!")
+        else:
+            return username_
