d6abd3f2a665c33c984fd696b83cc2587e273773
Dizi <58800976+DiziASP@users.noreply.github.com>
2022-04-09 15:26:30 +0700

    Minor Bug Fix

--- package/F16.py
+++ package/F16.py
@@ -2,11 +2,16 @@ from .base import*
 
 
 def save(user, game, kepemilikan, riwayat):
-    folder = input("Masukkan nama folder penyimpanan: ")
-    create_folder(folder)
-    write_csv(folder, user, "user")
-    write_csv(folder, game, "game")
-    write_csv(folder, kepemilikan, "kepemilikan")
-    write_csv(folder, riwayat, "riwayat")
-
-    input("Data berhasil tersimpan...")
+    while True:
+        try:
+            folder = input("Masukkan nama folder penyimpanan: ")
+            create_folder(folder)
+            write_csv(folder, user, "user")
+            write_csv(folder, game, "game")
+            write_csv(folder, kepemilikan, "kepemilikan")
+            write_csv(folder, riwayat, "riwayat")
+            input("Data berhasil tersimpan...")
+            return
+        except ValueError:
+            print("Tolong masukkan nama folder yang valid :>")
+            continue
