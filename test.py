1b5e1827da3197f0fbfa35ffc1ca8339322c6eb0
Dizi <58800976+DiziASP@users.noreply.github.com>
2022-04-12 12:40:34 +0700

    FIX BUG BISMILLAH ANJAY

--- test.py
+++ test.py
@@ -1,16 +1,15 @@
 from package import*
-from package.B02 import kerangajaib
 
 
-# def temp_load():
-#     user = csv_reader('dummy', "user")
-#     game = csv_reader('dummy', "game")
-#     kepemilikan = csv_reader('dummy', "kepemilikan")
-#     riwayat = csv_reader('dummy', "riwayat")
-#     return user, game, kepemilikan, riwayat
+def temp_load():
+    user = csv_reader('dummy', "user")
+    game = csv_reader('dummy', "game")
+    kepemilikan = csv_reader('dummy', "kepemilikan")
+    riwayat = csv_reader('dummy', "riwayat")
+    return user, game, kepemilikan, riwayat
 
 
-# user, game, kepemilikan, riwayat = temp_load()
-# search_my_game("6", game, kepemilikan)
+user, game, kepemilikan, riwayat = temp_load()
+login(user)
 
-kerangajaib()
+login()
